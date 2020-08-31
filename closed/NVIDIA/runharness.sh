0b907502d20ecec590993fb1d8c18555b4eeb985
eldritchjs <jschlessman@gmail.com>
2020-08-31 15:28:35 -0400

    Typo on ssd

--- closed/NVIDIA/runharness.sh
+++ closed/NVIDIA/runharness.sh
@@ -1,3 +1,3 @@
-make run_harness RUN_ARGS="--benchmarks=ssl-large,gnmt --scenarios=SingleStream --test-mode=PerformanceOnly"
+make run_harness RUN_ARGS="--benchmarks=ssd-large,gnmt --scenarios=SingleStream --test-mode=PerformanceOnly"
 make update_results
 
